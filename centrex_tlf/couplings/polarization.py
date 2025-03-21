9d19f0b0242e650299fff6d083b19fa805fe5c91
ograsdijk <o.grasdijk@gmail.com>
2025-03-21 16:37:10 -0500

    add __mul__ and __add__ methods to polarization

--- centrex_tlf/couplings/polarization.py
+++ centrex_tlf/couplings/polarization.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 
 import numpy as np
 import numpy.typing as npt
@@ -12,6 +13,13 @@ __all__ = [
 ]
 
 
+def format_value(val: complex) -> str:
+    # If there is no imaginary part, show only the real component.
+    if val.imag == 0:
+        return f"{val.real}"
+    return f"{val}"
+
+
 @dataclass
 class Polarization:
     vector: npt.NDArray[np.complex128]
@@ -19,6 +27,36 @@ class Polarization:
 
     def __repr__(self) -> str:
         return f"Polarization({self.name})"
+
+    def __mul__(self, value: int | float | complex) -> Self:
+        assert isinstance(value, (int, float, complex))
+        vec_new = self.vector * value
+        name_new = self._decompose(vec_new)
+        return self.__class__(vector=vec_new, name=name_new)
+
+    def __rmul__(self, value: int | float | complex) -> Self:
+        return self.__mul__(value)
+
+    def __add__(self, other: Self) -> Self:
+        vec_new = self.vector + other.vector
+        name_new = self._decompose(vec_new)
+        return self.__class__(vector=vec_new, name=name_new)
+
+    def normalize(self) -> Self:
+        vec_new = self.vector / np.linalg.norm(self.vector)
+        name_new = self._decompose(vec_new)
+        return self.__class__(vector=vec_new, name=name_new)
+
+    def _decompose(self, vector: npt.NDArray[np.complex128] | None) -> str:
+        if vector is None:
+            vector = self.vector
+        x, y, z = vector
+        x_str = format_value(x)
+        y_str = format_value(y)
+        z_str = format_value(z)
+
+        name = f"{x_str} X + {y_str} Y + {z_str} Z"
+        return name
 
 
 polarization_X = Polarization(np.array([1, 0, 0], dtype=np.complex128), "X")
